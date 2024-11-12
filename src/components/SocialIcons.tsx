5734ab4a541d3af75e3b53cbc251c9984f3ba8b6
solutionstack99 <solution.stack99@gmail.com>
2024-11-12 13:36:36 -0500

    Added YouTube icon function.

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -3,6 +3,7 @@ import { PrismicNextLink } from "@prismicio/next";
 
 const icons = {
   x: <XIcon />,
+  youTube: <YouTubeIcon />,
   email: <EmailIcon />,
 };
 
@@ -45,6 +46,21 @@ function XIcon() {
       viewBox="0 0 16 16"
     >
       <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+    </svg>
+  );
+}
+
+function YouTubeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="28"
+      height="28"
+      fill="white"
+      className="bi bi-youtube"
+      viewBox="0 0 16 16"
+    >
+      <path d="M8.051 1.999c-3.866 0-7.051 3.134-7.051 7s3.185 7 7.051 7 7.051-3.134 7.051-7-3.185-7-7.051-7zm0 12.5c-3.032 0-5.5-2.468-5.5-5.5s2.468-5.5 5.5-5.5 5.5 2.468 5.5 5.5-2.468 5.5-5.5 5.5zm-1.5-8.5v6l5-3-5-3z" />
     </svg>
   );
 }
